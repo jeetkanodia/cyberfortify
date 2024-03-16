f1d620e4ed2912aebd2a9483144eba27f4de72b9
jeet kanodia <jeetkanodia@gmail.com>
2024-03-16 16:38:10 +0530

    Add input field and state to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,31 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [data, setData] = useState("");
+
+  return (
+    <div
+      style={{
+        backgroundImage: "radial-gradient(rgb(42, 37, 65), rgb(0, 0, 0))",
+      }}
+      className="text-white w-full min-h-screen flex flex-col items-center"
+    >
+      <h1 className="text-white text-6xl m-10 mt-28">Cyber Fortify</h1>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+        nesciunt consequatur voluptas illum facere quibusdam id deserunt fuga
+        rerum officiis!
+      </div>
+      <input
+        type="text"
+        className="m-5 p-2 text-black"
+        placeholder="Enter website link"
+        value={data}
+        onChange={(e) => {
+          setData(e.target.value);
+        }}
+      />
+    </div>
+  );
 }
