b11814ce4e75bdd9d5ecb0d9ef76b320d1553b76
jeet kanodia <jeetkanodia@gmail.com>
2024-03-18 14:38:38 +0530

    done maybe

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const scanRoutes = require('./routes/scanRoutes');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const scanRoutes = require("./routes/scanRoutes");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(helmet());
-
-mongoose.connect('mongodb://localhost:27017/dast', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
   })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+);
 
-app.use('/api', scanRoutes);
+// mongoose
+//   .connect("mongodb://localhost:27017/dast", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to MongoDB:", err);
+//   });
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/api", scanRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-  
+  console.log(`Server running on port ${PORT}`);
+});
